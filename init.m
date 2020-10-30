ebb96ad54aa0df611d5c5d88551dbb061e8187a5
MichelangeloDF <df.michelangelo@gmail.com>
2020-10-30 15:40:31 +0100

    added subpath

---
+++ init.m
@@ -0,0 +1,20 @@
+%% Add paths
+%add paths to the init file
+addpath('./validation');
+addpath('./inputTimeseries');
+validateData() 
+data = readtable('S&P 500 Dati Storici.csv');
+
+%% data input
+% data input from CSV, AlphaVantage/datafeed
+
+% input of related information
+
+%% Data validation`
+
+
+
+%% Backtest 
+
+
+%% Analysis of the data of the backtest
---
+++ validation/validateData.m
@@ -0,0 +1,8 @@
+function [] = validateData()
+%VALIDATEDATA Summary of this function goes here
+%   Detailed explanation goes here
+
+    disp("i'im working" )
+
+end
+
