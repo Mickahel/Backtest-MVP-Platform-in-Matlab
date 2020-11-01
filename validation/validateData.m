45196522ff4bfc6b54a54f16d077f98e33985966
MichelangeloDF <df.michelangelo@gmail.com>
2020-11-01 19:30:36 +0100

    done data validation and cleaning

--- validation/validateData.m
+++ validation/validateData.m
@@ -18,8 +18,8 @@ function [financialDataObject] = validateData(timeseriesData, timeframe, origin)
     % 2) sort the data
     [formattedDates, sortOrder] = sort(timeseriesData.Date);   
     formattedPrices = formattedPrices(sortOrder,:);
-    [formattedPrices, missingData] = addMissingData(formattedPrices) ;
-    formattedPrices
+    [formattedPrices, missingData] = addMissingData(formattedPrices);
+    
     % 3) calculate the returns 
     formattedReturns = [NaN;diff(formattedPrices)]./formattedPrices*100;
     
@@ -48,24 +48,36 @@ function [prices, missingData] = addMissingData(prices)
     for positionIndex = 1:size(prices,1)
         %search for the first anomaly
         if prices(positionIndex) <= 0  || isnan(prices(positionIndex)) || isempty(prices(positionIndex))
-            % TODO: if it is the first element of the series
-            missingData= missingData+1; 
+            % TODO: if it is the first element of the series 
             missingDataIndex=0;
-            firstPrice = prices(positionIndex-1); 
-            while  ~isa(prices(positionIndex+missingDataIndex), 'double')  
+            
+            while  ~isa(prices(positionIndex+missingDataIndex), 'double')  || isnan(prices(positionIndex+missingDataIndex))
                 missingDataIndex=missingDataIndex+1;
                 missingData=missingData+1;
-            end   
-            lastPrice = prices(positionIndex+missingDataIndex+1);
-            % get the "m", the angular coefficient
-            m=(lastPrice-firstPrice)/(missingDataIndex-1);
-            coefficientToAdd = m/(missingDataIndex+1);
-            % adding the new data
-            for missingPriceIndex = 1:missingDataIndex
-                prices(positionIndex+missingPriceIndex) = firstPrice+coefficientToAdd*missingDataIndex;
+            end
+            
+            if missingDataIndex>0
+                % if it's at the beginning
+                if positionIndex==0 
+                    lastPrice = prices(positionIndex+missingDataIndex);
+                    firstPrice = lastPrice;
+                % if the last price is NaN, then use FirstPrice
+                elseif isnan(prices(positionIndex+missingDataIndex)) 
+                    firstPrice = prices(positionIndex-1);
+                    lastPrice = firstPrice;
+                else
+                    firstPrice = prices(positionIndex-1);
+                    lastPrice = prices(positionIndex+missingDataIndex);    
+                end
+                
+                % get the "m", the angular coefficient
+                m=(lastPrice-firstPrice)/(missingDataIndex+1);
+                % adding the new data
+                for missingPriceIndex = 0:missingDataIndex-1
+                    prices(positionIndex+missingPriceIndex) = firstPrice+m*(missingPriceIndex+1);
+                end
             end
         end
     end
-
 end
 
